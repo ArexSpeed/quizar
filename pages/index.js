f41a08d1d0b09d243a7dd7e2914fb273a88ff381
Arek Cichocki <arexspeed95@gmail.com>
2021-06-16 19:18:41 +0200

    add nav

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,31 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-start h-screen w-full mx-auto md:max-w-screen-md py-2 bg-gray-100 overflow-y-auto">
+    <>
       <Head>
         <title>Quizar</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <header className="relative flex flex-col justify-center items-center w-full h-1/3 mx-2 -my-2 rounded-big bg-gradient-to-r from-blue-400 to-purple-500">
+      <div className="relative flex flex-col items-center justify-start h-screen w-full mx-auto md:max-w-screen-md p-2 bg-gray-100 overflow-y-auto">
+      <div className="fixed bottom-0 inset-x-0 z-10 w-full h-20 px-4 mx-auto mb-2 shadow:md md:max-w-screen-md">
+      <nav className="w-full h-20 bg-white backdrop-filter backdrop-blur-sm bg-opacity-90 shadow-md rounded-xl">
+        <ul className="flex flex-row justify-between items-center">
+          <li className="w-full h-20 flex flex-col justify-center items-center">
+            <svg className="w-6 h-6 text-gray-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" /></svg>
+            Home
+          </li>
+          <li className="w-full h-20 flex flex-col justify-center items-center">
+            <svg className="w-6 h-6 text-gray-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" /></svg>
+            Stats
+          </li>
+          <li className="w-full h-20 flex flex-col justify-center items-center">
+            <svg className="w-6 h-6 text-gray-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" /></svg>
+            Profile
+          </li>
+        </ul>
+      </nav>
+      </div>
+      <header className="relative flex flex-col justify-center items-center w-full h-1/3 mx-2 rounded-big bg-gradient-to-r from-blue-400 to-purple-500">
         <div className="flex justify-center items-center rounded-full w-24 h-24 bg-white">
         <div className="w-22 h-22 rounded-full overflow-hidden flex justify-center">
             <img className="object-cover" src="https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
@@ -128,6 +146,7 @@ export default function Home() {
           </div>
         </section>
       </main>
-    </div>
+      </div>
+    </>
   )
 }
