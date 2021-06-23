4cd870ee524b16918164aa6646f28e263ab7b483
Arek Cichocki <arexspeed95@gmail.com>
2021-06-23 14:37:14 +0200

    add question to api

--- pages/add/questions.js
+++ pages/add/questions.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import { useState ,useRef } from 'react';
 import { useRouter } from 'next/router';
 
 const AddQuestions = () => {
   //const [questions, setQuestions] = useState([]);
   const router = useRouter();
   const categoryName = router.query.category;
+  const [formProcessing, setFormProcessing] = useState(false);
+  const [error, setError] = useState();
+  const addQuestionForm = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (formProcessing) return;
+    setError(null);
+    setFormProcessing(true);
+    const form = new FormData(addQuestionForm.current);
+    const payload = {
+      category: categoryName,
+      content: form.get('content'),
+      correctAnswer: form.get('correctAnswer'),
+      answer2: form.get('answer2'),
+      answer3: form.get('answer3'),
+      answer4: form.get('answer4')
+    };
+
+    const response = await fetch('/api/questions', {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      setFormProcessing(false);
+      router.push('/add/category');
+    } else {
+      const payload = await response.json();
+      setFormProcessing(false);
+      setError(payload.error);
+    }
+  };
+
 
   return (
     <div className="relative flex flex-col items-center justify-start h-screen w-full mx-auto md:max-w-screen-md p-2 bg-gray-100 overflow-x-hidden overflow-y-auto">
-     <form className="w-full flex flex-col justify-start items-center">
+     <form className="w-full flex flex-col justify-start items-center" onSubmit={handleSubmit} ref={addQuestionForm}>
         <div className="w-full h-24 my-2 flex flex-row justify-start items-center bg-white shadow-md rounded-md">
           <textarea className="p-2 text-xl w-full h-full overflow-x-hidden" type="text" id="content" name="content" placeholder="Question" required />
         </div>
@@ -16,21 +53,23 @@ const AddQuestions = () => {
           <input className="px-2 text-xl w-full h-full" type="text" id="correctAnswer" name="correctAnswer" placeholder="Correct Answer" required />
         </div>
         <div className="w-full h-16 my-2 flex flex-row justify-start items-center bg-white shadow-md rounded-md">
-          <input className="px-2 text-xl w-full h-full" type="text" id="Answer2" name="Answer2" placeholder="Answer 2" required />
+          <input className="px-2 text-xl w-full h-full" type="text" id="answer2" name="answer2" placeholder="Answer 2" required />
         </div>
         <div className="w-full h-16 my-2 flex flex-row justify-start items-center bg-white shadow-md rounded-md">
-          <input className="px-2 text-xl w-full h-full" type="text" id="Answer3" name="Answer3" placeholder="Answer 3" required />
+          <input className="px-2 text-xl w-full h-full" type="text" id="answer3" name="answer3" placeholder="Answer 3" required />
         </div>
         <div className="w-full h-16 my-2 flex flex-row justify-start items-center bg-white shadow-md rounded-md">
-          <input className="px-2 text-xl w-full h-full" type="text" id="Answer4" name="Answer4" placeholder="Answer 4" required />
+          <input className="px-2 text-xl w-full h-full" type="text" id="answer4" name="answer4" placeholder="Answer 4" required />
         </div>
+        <button type="submit" className="w-1/2 h-20 my-2 flex flex-row justify-between items-center bg-green-100 rounded-md">
+          <div className="px-2 mx-2 w-8 h-8 rounded-full flex justify-center items-center bg-green-200">
+          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" /></svg>
+            </div>
+          <span className="px-2">Add Question to {categoryName}</span>
+        </button>
     </form>
-     <button type="submit" className="w-1/2 h-20 my-2 flex flex-row justify-between items-center bg-green-100 rounded-md">
-      <div className="px-2 mx-2 w-8 h-8 rounded-full flex justify-center items-center bg-green-200">
-      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" /></svg>
-        </div>
-      <span className="px-2">Add Question to {categoryName}</span>
-     </button>
+    {error && <div className="w-full h-12 my-2 text-center text-md bg-red-500 text-white flex flex-row justify-start items-center shadow-md rounded-md">{error}</div>}
+     
     
   </div>
   )
--- pages/api/questions.js
+++ pages/api/questions.js
@@ -4,7 +4,50 @@ export default async (req, res) => {
   const { db } = await connectToDatabase();
   const query = req.query;
 
-  const data = await db.collection("questions").find({"category": query.category}).sort({_id: 1}).toArray();
+  switch (req.method) {
+    case 'GET': {
+      const data = await db.collection("questions").find({"category": query.category}).sort({_id: 1}).toArray();
+      res.json(data);
 
-  return res.json(data);
+      break;
+    }
+    case 'POST': {
+      try {
+        const payload = req.body;
+        //console.log(payload, 'body')
+        const question = await db.collection('questions').insertOne(
+          {
+            category: payload.category,
+            content: payload.content,
+            answers: [
+              {
+                valid: true,
+                content: payload.correctAnswer
+              },
+              {
+                valid: false,
+                content: payload.answer2
+              },
+              {
+                valid: false,
+                content: payload.answer3
+              },
+              {
+                valid: false,
+                content: payload.answer4
+              },
+            ]
+          }
+        );
+        res.status(200).json({ status: 'created', question});
+      } catch (error) {
+        res.status(422).json({ status: 'not_created', error });
+      }
+      break;
+    }
+
+    default:
+      res.status(400);
+  }
+
 };
